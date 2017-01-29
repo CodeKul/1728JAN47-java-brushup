3f6818e2b44e91207775ca4b7d3aae23a46d925a
aniruddha <aniruddha.kudalkar@gmail.com>
2017-01-29 16:42:55 +0530

    initial commit

---
+++ src/main/java/com/codekul/java/brushup/Main.java
@@ -0,0 +1,10 @@
+package com.codekul.java.brushup;
+
+/**
+ * Created by aniruddha on 28/1/17.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.printf("\n Strting from here ...");
+    }
+}
